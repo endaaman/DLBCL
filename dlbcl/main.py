9176f72967224c1cdaeda06b72b43c7a6fb0ebf5
endaaman <ken@endaaman.com>
2025-06-05 19:05:47 +0900

    cluster

--- dlbcl/main.py
+++ dlbcl/main.py
@@ -14,7 +14,6 @@ from matplotlib import pyplot as plt, colors as mcolors
 from matplotlib.colors import LinearSegmentedColormap, Normalize
 import seaborn as sns
 import h5py
-import umap
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import DBSCAN
 from sklearn.decomposition import PCA
@@ -23,6 +22,7 @@ import hdbscan
 import torch
 import timm
 from umap import UMAP
+from gigapath import slide_encoder
 
 from .utils import BaseMLCLI, BaseMLArgs
 
@@ -89,7 +89,7 @@ class CLI(BaseMLCLI):
         features = np.array(features)
         print('features', features.shape)
 
-        o = str(self.dataset_dir / 'global_slide_features.h5')
+        o = str(self.dataset_dir / 'slide_features.h5')
         with h5py.File(o, 'w') as f:
             f.create_dataset('features', data=features)
             f.create_dataset('names', data=df['name'].values)
@@ -98,35 +98,149 @@ class CLI(BaseMLCLI):
         print(f'wrote {o}')
 
     class UMAPArgs(CommonArgs):
-        key: str = 'DBSCAN cluster'
+        keys: list[str] = param(['DBSCAN cluster'])
 
     def run_umap(self, a:UMAPArgs):
         df  = pd.read_csv('./data/DLBCL-Morph/DBSCAN_clusters_with_ids.csv', index_col=0)
-        with h5py.File('./data/DLBCL-Morph/global_slide_features.h5', 'r') as f:
+        keys = a.keys
+        if 'ALL' in a.keys:
+            keys = list(df.columns)
+        with h5py.File('./data/DLBCL-Morph/slide_features.h5', 'r') as f:
             features = f['features'][()]
             names = f['names'][()]
             orders = f['orders'][()]
-            labels = []
+            data = []
             for name, order in zip(names, orders):
                 label = f'{name.decode("utf-8")}_{order}'
-                labels.append(df.loc[label, a.key])
+                v = {}
+                for key in keys:
+                    v[key] = df.loc[label, key]
+                data.append(v)
 
-        labels = pd.DataFrame({'labels': labels})
+        df_labels = pd.DataFrame(data)
 
-        umap = UMAP()
-        embs = umap.fit_transform(features)
-        unique_labels = sorted(np.unique(labels))
-        cmap = plt.get_cmap('tab20')
-        for i, label in enumerate(unique_labels):
-            mask = labels['labels'] == label
-            if isinstance(label, (int, np.integer)):
-                c = 'gray' if label < 0 else cmap(label)
-            else:
-                c = cmap(i)
-            print(c)
-            plt.scatter(embs[mask,0], embs[mask,1], c=c, label=f'{a.key} {label}')
-        plt.savefig(f'./out/umap_{a.key}.png')
-        # plt.show()
+        for key in keys:
+            plt.close()
+            labels = df_labels[key]
+            umap = UMAP()
+            embs = umap.fit_transform(features)
+            unique_labels = sorted(np.unique(labels))
+            cmap = plt.get_cmap('tab20')
+            for i, label in enumerate(unique_labels):
+                mask = labels == label
+                if isinstance(label, (int, np.integer)):
+                    c = 'gray' if label < 0 else cmap(label)
+                else:
+                    c = cmap(i)
+                plt.scatter(embs[mask,0], embs[mask,1], c=c, label=f'{key} {label}')
+            plt.legend()
+            plt.savefig(f'./out/umap_{key.replace(" ", "_")}.png')
+        plt.show()
+
+
+
+    class ClusterArgs(CommonArgs):
+        target: str = Field('cluster', s='-T')
+        noshow: bool = False
+
+    def run_cluster(self, a):
+        with h5py.File('./data/DLBCL-Morph/slide_features.h5', 'r') as f:
+            features = f['features'][:]
+            df = pd.DataFrame({
+                'name': [int((v.decode('utf-8'))) for v in f['names'][:]],
+                'filename': [v.decode('utf-8') for v in f['filenames'][:]],
+                'order': f['orders'][:],
+            })
+
+        df_clinical = pd.read_excel('./data/clinical_data_cleaned.xlsx', index_col=0)
+        df = pd.merge(
+            df,
+            df_clinical,
+            left_on='name',
+            right_index=True,
+            how='left'
+        )
+
+        print('Loaded features', features.shape)
+        scaler = StandardScaler()
+        scaled_features = scaler.fit_transform(features)
+        # scaled_features = features
+
+        print('UMAP fitting...')
+        reducer = umap.UMAP(
+                n_neighbors=10,
+                min_dist=0.05,
+                n_components=2,
+                metric='cosine',
+                random_state=a.seed,
+                n_jobs=1,
+            )
+        embedding = reducer.fit_transform(scaled_features)
+        print('Loaded features', features.shape)
+
+        if a.target in [
+                'HDBSCAN',
+                'CD10 IHC', 'MUM1 IHC', 'HANS', 'BCL6 FISH', 'MYC FISH', 'BCL2 FISH',
+                'ECOG PS', 'LDH', 'EN', 'Stage', 'IPI Score',
+                'IPI Risk Group (4 Class)', 'RIPI Risk Group', 'Follow-up Status',
+                ]:
+            mode = 'categorical'
+        elif a.target in ['MYC IHC', 'BCL2 IHC', 'BCL6 IHC', 'Age', 'OS', 'PFS']:
+            mode = 'numeric'
+        else:
+            raise RuntimeError('invalid target', a.target)
+
+
+        plt.figure(figsize=(10, 8))
+        marker_size = 15
+
+        if mode == 'categorical':
+            labels = df[a.target].fillna(-1)
+            n_labels = len(set(labels))
+            cmap = plt.cm.viridis
+
+            noise_mask = labels == -1
+            valid_labels = sorted(list(set(labels[~noise_mask])))
+            norm = plt.Normalize(min(valid_labels or [0]), max(valid_labels or [1]))
+            for label in valid_labels:
+                mask = labels == label
+                color = cmap(norm(label))
+                plt.scatter(
+                    embedding[mask, 0], embedding[mask, 1], c=[color],
+                    s=marker_size, label=f'{a.target} {label}'
+                )
+
+            if np.any(noise_mask):
+                plt.scatter(
+                    embedding[noise_mask, 0], embedding[noise_mask, 1], c='gray',
+                    s=marker_size, marker='x', label='Noise/NaN',
+                )
+
+        else:
+            values = df[a.target]
+            norm = Normalize(vmin=values.min(), vmax=values.max())
+            values = values.fillna(-1)
+            has_value = values > 0
+            cmap = plt.cm.viridis
+            scatter = plt.scatter(embedding[has_value, 0], embedding[has_value, 1], c=values[has_value],
+                                  s=marker_size, cmap=cmap, norm=norm, label=a.target,)
+            if np.any(has_value):
+                plt.scatter(embedding[~has_value, 0], embedding[~has_value, 1], c='gray',
+                            s=marker_size, marker='x', label='NaN')
+            cbar = plt.colorbar(scatter)
+            cbar.set_label(a.target)
+
+        plt.title(f'UMAP + {a.target}')
+        plt.xlabel('UMAP Dimension 1')
+        plt.ylabel('UMAP Dimension 2')
+        # plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+        plt.legend()
+        plt.tight_layout()
+        os.makedirs('out/morph', exist_ok=True)
+        name = a.target.replace(' ', '_')
+        plt.savefig(f'out/morph/umap_{name}.png')
+        if not a.noshow:
+            plt.show()
 
 
     class GlobalClusterArgs(CommonArgs):
